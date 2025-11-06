ab6560466641c71e240b2a87924ec6de3ef1fe11
hatice-sp <143039103+hatice-sp@users.noreply.github.com>
2025-11-07 00:41:37 +0300

    Backend: Flask AI URL

--- backend/ChatEmotionBackend/Controllers/MessageController.cs
+++ backend/ChatEmotionBackend/Controllers/MessageController.cs
@@ -35,10 +35,9 @@ namespace ChatEmotionBackend.Controllers
             {
                 // Python microservice'e POST at
                 var response = await _httpClient.PostAsJsonAsync(
-     "https://chat-emotion-app-ox9j.onrender.com/analyze",
-     new { text = message.Text }
- );
-
+                    "http://127.0.0.1:5000/analyze",  // Python servis URL
+                    new { text = message.Text }
+                );
 
                 if (response.IsSuccessStatusCode)
                 {
@@ -65,8 +64,7 @@ namespace ChatEmotionBackend.Controllers
         public IActionResult GetMessages()
         {
             var messages = _context.Messages
-                .Select(m => new
-                {
+                .Select(m => new {
                     m.Id,
                     m.UserId,
                     m.Text,
@@ -78,10 +76,10 @@ namespace ChatEmotionBackend.Controllers
         [HttpDelete("reset")]
         public async Task<IActionResult> ResetDatabase()
         {
-            var messages = _context.Messages.ToList();
-            _context.Messages.RemoveRange(messages);
-            await _context.SaveChangesAsync();
-            return Ok("Veritabanı temizlendi.");
+          var messages = _context.Messages.ToList();
+         _context.Messages.RemoveRange(messages);
+          await _context.SaveChangesAsync();
+          return Ok("Veritabanı temizlendi.");
         }
 
     }
